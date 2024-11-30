aaf9a20d0635d6d2f892ca1ed56b02884559f61f
Ayomide Aderojuola <aderojuolaayomide57@gmail.com>
2024-11-30 18:34:24 +0100

    Goldsky subgraph integration

--- ethenacredit-dapp/src/util/constants/contractAddresses.ts
+++ ethenacredit-dapp/src/util/constants/contractAddresses.ts
@@ -5,3 +5,4 @@ export const USDe: `0x${string}` = "0xf805ce4F96e0EdD6f0b6cd4be22B34b92373d696";
 export const sUSDe: `0x${string}` = "0x1B6877c6Dac4b6De4c5817925DC40E2BfdAFc01b";
 export const USDeOft: `0x${string}` = "0x426E7d03f9803Dd11cb8616C65b99a3c0AfeA6dE";
 export const sUSDeOft: `0x${string}` = "0x80f9Ec4bA5746d8214b3A9a73cc4390AB0F0E633";    
+
